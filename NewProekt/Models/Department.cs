89c0f1ad25c78d5d54f728d882a46f1d45f3e25e
SahibAkbar <sahibaa@code.edu.az>
2021-08-20 16:22:46 +0400

    Department and Employee class added

---
+++ NewProekt/Models/Department.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewProject.Models
+{
+    class Department
+    {
+        private string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                if (Name.Length>=2)
+                {
+                    Name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Department adi minimum 2 herfden ibaret olmalidir");
+                }
+            }
+        }
+
+        public int WorkerLimit
+        {
+            get
+            {
+                return WorkerLimit;
+            }
+            set
+            {
+                if (value>=1)
+                {
+                    WorkerLimit = value;
+                }
+                else
+                {
+                    Console.WriteLine("Departmentde minimum  var ola bilecek isci sayi 1 ola biler");
+                }
+            }
+        }
+
+        public int SalaryLimit
+        {
+            get
+            {
+                return SalaryLimit;
+            }
+            set
+            {
+                if (value>=250)
+                {
+                    SalaryLimit = value;
+                }
+                else
+                {
+                    Console.WriteLine("Departamentde butun isceleri ayliq cemi verilecek mebleg minimum 250 ola biler");
+                }
+            }
+        }
+
+        public List<Employee>Employees { get; set; }
+
+        public double CalcSalaryAverage()
+        {
+            double sum = 0;
+            double average = 0;
+            foreach (var item in Employees)
+            {
+                sum += item.Salary;
+            }
+            if (Employees.Count !=0)
+            {
+                average = sum / Employees.Count;
+                return average;
+            }
+            else
+            {
+                return 0;
+            }
+        }                                                  
+    }
+}
---
+++ NewProekt/Models/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewProject.Models
+{
+    class Employee
+    {
+        private static int _counter = 1000;
+
+        public string No { get; set; }
+
+        public Employee()
+        {
+            _counter++;
+            No = DepartmentName.Substring(0, 2).ToUpper() + _counter;
+        }
+
+        public string Fullname { get; set; }
+
+        public string Position
+        {
+            get
+            {
+                return Position;
+            }
+            set
+            {
+                if (Position.Length>=2)
+                {
+                    Position = value;
+                }
+                else
+                {
+                    Console.WriteLine("Iscinin vezifesi (minimum 2 herfden ibaret olmalidir)");
+                }
+            }
+        }
+
+
+        public int Salary
+        {
+            get
+            {
+                return Salary;
+            }
+            set
+            {
+                if (value>=250)
+                {
+                    Salary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Iscinin maasi 250-den asagi ola bilmez");
+                }
+            }
+        }
+
+        public string DepartmentName { get; set; }
+    }
+}
